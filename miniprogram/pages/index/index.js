c3d04cf2a76723437cb252274d7eb0603323a0eb
uicky <2928053023@qq.com>
2024-05-14 19:05:13 +0800

    对于创建用户的初步尝试

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,36 +1,6 @@
-const { envList } = require("../../envList");
-const { QuickStartPoints, QuickStartSteps } = require("./constants");
-
 Page({
-  data: {
-    knowledgePoints: QuickStartPoints,
-    steps: QuickStartSteps,
-  },
-
-  copyCode(e) {
-    const code = e.target?.dataset?.code || '';
-    wx.setClipboardData({
-      data: code,
-      success: () => {
-        wx.showToast({
-          title: '已复制',
-        })
-      },
-      fail: (err) => {
-        console.error('复制失败-----', err);
-      }
-    })
-  },
-
-  discoverCloud() {
-    wx.switchTab({
-      url: '/pages/examples/index',
-    })
-  },
-
-  gotoGoodsListPage() {
-    wx.navigateTo({
-      url: '/pages/goods-list/index',
-    })
-  },
+  submit: function (e) {
+    // 处理表单提交逻辑
+    console.log(e);
+  }
 });
